4624f91548279ca8a915b774d89c1ba285e762b0
nalwebdevelopement <naliniu91@gmail.com>
2024-09-28 20:30:32 -0400

    top bar and nav bar component added

---
+++ src/Components/Nav.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import "./Nav.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
+
+function Nav() {
+  return (
+    <div>
+      <div className="topbar">
+        <navbar class="navbar">
+          {/* <div class="logo">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/H-E-B_logo.svg/1024px-H-E-B_logo.svg.png" alt="HEB Logo"/>
+        </div> */}
+          <div className="home">
+            {/* <FontAwesomeIcon icon="fa-solid fa-house" />   */}
+
+            <span>
+              <FontAwesomeIcon icon={faHouse} />
+            </span>
+          </div>
+          <div class="search-bar">
+            <input type="text" placeholder="Search" id="search" />
+            <button>
+              <i class="fas fa-search"></i>
+            </button>
+          </div>
+          <div class="right-options">
+            <span>Select a pickup time</span>
+
+            <i class="fas fa-shopping-cart"></i>
+          </div>
+        </navbar>
+      </div>
+
+      <navbar className="nav-bar">
+        <div className="nav-main">
+          <div className="nav">
+            <img
+              src="https://media4.giphy.com/media/fYNSIDot0a7AZuDWhl/giphy.gif?cid=6c09b952g58wltijf7vccg44h6eqaxgv7pcz483j28l4nfk4&ep=v1_gifs_search&rid=giphy.gif&ct=g"
+              alt="img-main"
+              style={{ width: "100px", height: "100px", borderRadius: "50%" }}
+            />
+            <p> Shop</p>
+            <p> Receipes</p>
+            <p> Weekly Ad</p>
+            <p> Savings</p>
+            <p> Pharmancy</p>
+            <p> Wellness</p>
+          </div>
+          <div className="nav-right">
+            <p> Login/Register</p>
+            <p> Lists</p>
+            <p> Help&FAQ</p>
+          </div>
+        </div>
+      </navbar>
+    </div>
+  );
+}
+
+export default Nav;
